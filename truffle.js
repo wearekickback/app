d8ec545e79f0bdaeeee1c6d9ea641f565ca27374
Makoto <inouemak@googlemail.com>
2018-09-13 18:23:37 +0100

    Fix wrong indentation

--- truffle.js
+++ truffle.js
@@ -1,10 +1,9 @@
 module.exports = {
-    networks: {
-      development: {
-        host: "localhost",
-        port: 8545,
-        network_id: "*",
-      }
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
     }
   }
-  
+}
