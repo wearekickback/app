6b7339fd8c5daecfbd70a8acc4b490270ee73c71
Jeff Lau <jeff-lau@live.com>
2018-09-13 17:35:00 +0200

    Add header html

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,7 +2,19 @@ import React from 'react'
 import styled from 'react-emotion'
 
 const HeaderContainer = styled('header')``
+const Notifications = styled('div')``
+const Account = styled('div')``
+const Avatar = styled('img')``
 
-const Header = () => <HeaderContainer />
+const Header = () => (
+  <HeaderContainer>
+    <h1>Kickback</h1>
+    <Notifications>Notification</Notifications>
+    <Account>
+      <AccountAddress>vitalik.eth</AccountAddress>
+      <Avatar />
+    </Account>
+  </HeaderContainer>
+)
 
 export default Header
