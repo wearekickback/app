f05a20b36b87ace93a674872ebef5a09c071bbc6
Makoto <inouemak@googlemail.com>
2018-09-13 14:29:56 +0100

    Fix broken migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,22 @@
-var Deployer  = artifacts.require("@noblocknoparty/contracts/Deployer.sol")
+const util = require('web3-utils');
+const Deployer  = artifacts.require("@noblocknoparty/contracts/Deployer.sol")
+
+const getEvents = async (result, eventName) => {
+  const events = result.logs.filter(({ event }) => event === eventName)
+  return events
+}
 
 module.exports = async function(deployer) {
   await deployer.deploy(Deployer);
   const contract = await Deployer.deployed()
+  console.log('deployer', contract.address)
   const result = await contract.deploy(
     'test',
-    toHex(toWei('0.02')),
-    toHex(2),
-    toHex(60 * 60 * 24 * 7),
+    util.toHex(util.toWei('0.02')),
+    util.toHex(2),
+    util.toHex(60 * 60 * 24 * 7),
     'encKey'
   )
+  const events = await getEvents(result, 'NewParty')
+  console.log('party', events)
 }
