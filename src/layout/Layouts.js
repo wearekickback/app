43b6e255f309a5e34921c92acd1ea585b231907a
Jeff Lau <jeff-lau@live.com>
2018-09-21 16:27:27 +0100

    Fix bug

--- src/layout/Layouts.js
+++ src/layout/Layouts.js
@@ -3,13 +3,13 @@ import React, { Fragment } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import ErrorBox from '../components/ErrorBox'
-import { getNetworkError } from '../api/ethers'
+import { getNetworkError } from '../api/web3'
 
 const DefaultLayout = ({ children }) => (
   <Fragment>
     <Header />
 
-    {getNetworkError ? <ErrorBox>{`${networkError}`}</ErrorBox> : null}
+    {getNetworkError() ? <ErrorBox>{`${getNetworkError}`}</ErrorBox> : null}
     {children}
     <Footer />
   </Fragment>
