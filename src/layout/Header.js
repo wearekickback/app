9c74be7de423bc620216cc9a413936cd490f183f
Jeff Lau <jeff-lau@live.com>
2018-09-24 17:07:03 +0100

    Added logo and link back to header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import styled from 'react-emotion'
+import { Link as DefaultLink } from 'react-router-dom'
 import SignIn from '../components/Modal/ToggleModal'
+import LogoIconDefault from '../components/Icons/Logo'
 
 const HeaderContainer = styled('header')`
   width: 100%;
-  height: 50px;
+  height: 70px;
   display: flex;
   background: #6e76ff;
   justify-content: space-between;
+  align-items: center;
+  color: white;
+`
+
+const Link = styled(DefaultLink)`
+  display: flex;
+  align-items: center;
+`
+
+const LogoIcon = styled(LogoIconDefault)`
+  margin-right: 5px;
 `
 
 const Logo = styled('h1')`
   margin: 0;
+  font-size: 22px;
+
+  a {
+    font-family: Helvetica;
+    color: white;
+  }
 `
 const RightBar = styled('div')`
   display: flex;
@@ -23,7 +42,12 @@ const Avatar = styled('img')``
 
 const Header = () => (
   <HeaderContainer>
-    <Logo>Kickback</Logo>
+    <Logo>
+      <Link to="/">
+        <LogoIcon />
+        Kickback
+      </Link>
+    </Logo>
     <RightBar>
       <Notifications>Notification</Notifications>
       <Account>
