f1dc7677c3f488352ec05da8dfb8491902065957
Ramesh Nair <ram@hiddentao.com>
2018-10-11 20:06:56 +0100

    fix ids

--- src/graphql/ids.js
+++ src/graphql/ids.js
@@ -1,15 +1,21 @@
 import _ from 'lodash'
 
 const mappings = {
-  Party: 'address',
-  Participant: 'user.address',
-  SocialMedia: 'type',
+  Party: ['address'],
+  Participant: ['user.address'],
+  SocialMedia: ['type','value'],
 }
 
 export const dataIdFromObject = o => {
   const { __typename: type } = o
 
-  const id = _.get(o, mappings[type]) || JSON.stringify(o)
+  let id
+  const mapProps = mappings[type]
+  if (mapProps) {
+    id = mapProps.reduce((str, p) => `${str}${_.get(o, p, '')}`, '')
+  } else {
+    id = JSON.stringify(o)
+  }
 
-  return `${type}:${id}`
+  return id
 }
