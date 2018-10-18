9ea20f0201c7d7eb0f5fd2a713e3c1af59588a0d
Makoto Inoue <2630+makoto@users.noreply.github.com>
2018-10-18 11:23:44 +0100

    Admin can update their metadata (#8)
    * basic input validation
    
    * allow anything except numbers for name
    
    * only deploy to dev if on dev branch
    
    * Fixed graphql error for PartyMeta"
    
    * Remove log and fixed warning
    
    * Remove unused func
    
    * Remove console.log

--- src/routes/CreatePendingEvent.js
+++ src/routes/CreatePendingEvent.js
@@ -1,109 +1,25 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
-import SafeMutation from '../components/SafeMutation'
-import Button from '../components/Forms/Button'
+import PartyForm from '../components/SingleEvent/Admin/PartyForm'
 import { CreatePendingParty } from '../graphql/mutations'
 
 class Create extends Component {
-  state = {
-    name: '',
-    description: '',
-    location: '',
-    date: '',
-    image: '',
-    deposit: '0.02',
-    limitOfParticipants: 20,
-  }
-
   render() {
-    const {
-      name,
-      description,
-      location,
-      date,
-      image,
-      deposit,
-      limitOfParticipants
-    } = this.state
-
     return (
-      <div className="App">
+      <>
         <h1>Create a new party</h1>
-        <div>
-          <label>Name</label>
-          <input
-            value={name}
-            onChange={e => this.setState({ name: e.target.value })}
-            type="text"
-            placeholder="Name of the event"
-          /><br/>
-          <label>Description</label>
-          <textarea
-            value={description}
-            onChange={e => this.setState({ description: e.target.value })}
-            type="text"
-            placeholder="Description of the event"
-          >{description}</textarea><br/>
-          <label>Location</label>
-          <input
-            value={location}
-            onChange={e => this.setState({ location: e.target.value })}
-            type="text"
-            placeholder="Location of the event"
-          /><br/>
-          <label>Dates</label>
-          <input
-            value={date}
-            onChange={e => this.setState({ date: e.target.value })}
-            type="text"
-            placeholder="Dates of the event"
-          /><br/>
-          <label>Image</label>
-          <input
-            value={image}
-            onChange={e => this.setState({ image: e.target.value })}
-            type="text"
-            placeholder="URL to image for the event"
-          /><br/>
-          <label>Commitment</label>
-          <input
-            value={deposit}
-            onChange={e => this.setState({ deposit: e.target.value })}
-            type="text"
-            placeholder="ETH"
-          /><br/>
-          <label>Limit of participants</label>
-          <input
-            value={limitOfParticipants}
-            onChange={e => this.setState({ limitOfParticipants: e.target.value })}
-            type="text"
-            placeholder="number of participants"
-          /><br/>
-        </div>
-
-        <SafeMutation
-          mutation={CreatePendingParty}
-          resultKey='id'
-          variables={{ meta: { name, description, location, date, image } }}
+        <PartyForm
           onCompleted={this._onCreated}
-        >
-          {createPendingParty => (
-            <div>
-              <Button onClick={createPendingParty} analyticsId='Create Pending Event'>
-                Create pending event
-              </Button>
-            </div>
-          )}
-        </SafeMutation>
-      </div>
+          mutation={CreatePendingParty}
+        />
+      </>
     )
   }
-
-  _onCreated = ({ id }) => {
-    const { deposit, limitOfParticipants } = this.state
-
-    this.props.history.push(`/deploy?id=${id}&deposit=${deposit}&limitOfParticipants=${limitOfParticipants}`)
+  _onCreated = ({ id }, deposit, limitOfParticipants) => {
+    this.props.history.push(
+      `/deploy?id=${id}&deposit=${deposit}&limitOfParticipants=${limitOfParticipants}`
+    )
   }
 
   _buildHandler = ({ createParty, createPendingParty }) => {
--- src/routes/SingleEventAdmin.js
+++ src/routes/SingleEventAdmin.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import SingleEventWrapper from '../components/SingleEvent/SingleEventWrapper'
 import SetLimit from '../components/SingleEvent/SetLimit'
 import Clear from '../components/SingleEvent/Clear'
+import UpdatePartyMeta from '../components/SingleEvent/Admin/UpdatePartyMeta'
 
 class SingleEvent extends Component {
   state = {
@@ -20,6 +21,7 @@ class SingleEvent extends Component {
       <Fragment>
         <SetLimit address={address} />
         <Clear address={address} />
+        <UpdatePartyMeta address={address} />
         <SingleEventWrapper
           handleSearch={this.handleSearch}
           search={this.state.search}
