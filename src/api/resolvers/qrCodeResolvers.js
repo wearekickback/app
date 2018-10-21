8421d78026404ac60744904ec48ab9bdb4ada98c
Makoto <inouemak@googlemail.com>
2018-10-21 22:36:15 +0100

    Small refactoring

--- src/api/resolvers/qrCodeResolvers.js
+++ src/api/resolvers/qrCodeResolvers.js
@@ -4,15 +4,12 @@ export const defaults = {}
 const resolvers = {
   Query: {
     async scanQRCodeSupported() {
-      let supported = false;
       const web3 = await getWeb3()
-      if(web3.currentProvider.scanQRCode){
-        supported = true
-      }
       return {
-        supported:supported,
+        supported:!!web3.currentProvider.scanQRCode,
         __typename: 'QRCodeSupported'
       }
+      return !!web3.currentProvider.scanQRCode
     },
     async scanQRCode() {
       const web3 = await getWeb3()
