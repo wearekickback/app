2e5f5c0cf131a217a655384242e02ea522a90caf
Jeff Lau <jeff-lau@live.com>
2018-10-25 19:40:11 +0100

    Add download to csv functionality

--- src/routes/ParticipantTableList.js
+++ src/routes/ParticipantTableList.js
@@ -7,6 +7,7 @@ import { amInAddressList } from '../utils/parties'
 import { PartyVerboseQuery } from '../graphql/queries'
 
 import { Table, Tbody, TH, TR, TD } from '../components/Table'
+import Button from '../components/Forms/Button'
 import ErrorBox from '../components/ErrorBox'
 import SafeQuery from '../components/SafeQuery'
 import EventFilters from '../components/SingleEvent/EventFilters'
@@ -47,6 +48,49 @@ class SingleEventWrapper extends Component {
     this.setState({
       search: event.target.value
     })
+  }
+
+  downloadCSV(csv, filename) {
+    let csvFile
+    let downloadLink
+
+    // CSV FILE
+    csvFile = new Blob([csv], { type: 'text/csv' })
+
+    // Download link
+    downloadLink = document.createElement('a')
+
+    // File name
+    downloadLink.download = filename
+
+    // We have to create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile)
+
+    // Make sure that the link is not displayed
+    downloadLink.style.display = 'none'
+
+    // Add the link to your DOM
+    document.body.appendChild(downloadLink)
+
+    // Lanzamos
+    downloadLink.click()
+  }
+
+  exportTableToCSV(html, filename) {
+    var csv = []
+    var rows = document.querySelectorAll('table tr')
+
+    for (var i = 0; i < rows.length; i++) {
+      var row = [],
+        cols = rows[i].querySelectorAll('td, th')
+
+      for (var j = 0; j < cols.length; j++) row.push(cols[j].innerText)
+
+      csv.push(row.join(','))
+    }
+
+    // Download CSV
+    this.downloadCSV(csv.join('\n'), filename)
   }
 
   render() {
@@ -103,6 +147,14 @@ class SingleEventWrapper extends Component {
 
                 return (
                   <TableList>
+                    <Button
+                      onClick={() => {
+                        const html = document.querySelector('table').outerHTML
+                        this.exportTableToCSV(html, 'event.csv')
+                      }}
+                    >
+                      Download as CSV
+                    </Button>
                     <EventFilters handleSearch={handleSearch} />
                     <Table>
                       <Tbody>
