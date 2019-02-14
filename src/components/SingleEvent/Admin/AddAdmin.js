5f286e649dd4206471497bd260c26b1013ca70d6
Jeff Lau <jeff-lau@live.com>
2019-02-14 17:50:23 +0800

    Refactor AddAdmin component

--- src/components/SingleEvent/Admin/AddAdmin.js
+++ src/components/SingleEvent/Admin/AddAdmin.js
@@ -1,52 +1,91 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import styled from 'react-emotion'
+import Button from '../../Forms/Button'
+import TextInput from '../../Forms/TextInput'
+import Label from '../../Forms/Label'
 
 import ChainMutation, { ChainMutationButton } from '../../ChainMutation'
 import { ADD_PARTY_ADMINS } from '../../../graphql/mutations'
 import { PARTY_QUERY } from '../../../graphql/queries'
 
-const Form = styled('div')``
+const AddPartyAdminsContainer = styled('div')`
+  margin-bottom: 20px;
+`
 
-class AddAdmin extends Component {
-  state = {}
+const AddAdminInputContainer = styled('div')`
+  display: flex;
+  margin-bottom: 20px;
+`
 
-  render() {
-    const { address } = this.props
-    const { userAddresses } = this.state
+const AddAdminInput = styled(TextInput)`
+  margin: 0;
+  margin-right: 20px;
+`
 
-    return (
-      <ChainMutation
-        mutation={ADD_PARTY_ADMINS}
-        resultKey="addAdmins"
-        variables={{
-          address,
-          userAddresses: (userAddresses || '').split('\n').map(s => s.trim())
-        }}
-        refetchQueries={[{ query: PARTY_QUERY, variables: { address } }]}
-        onCompleted={() => this.setState({ userAddresses: null })}
-      >
-        {(mutate, result) => (
-          <Form>
-            <label>User:</label>
-            <textarea
+const Add = styled(Button)``
+
+function AddAdmin({ address }) {
+  const [userAddresses, setAddresses] = useState([''])
+  const pushAddress = address => setAddresses([...userAddresses, address])
+  const setAddress = (index, address) => {
+    const newArray = [...userAddresses]
+    newArray[index] = address
+    setAddresses(newArray)
+  }
+  const removeAddress = index => {
+    const newArray = [...userAddresses].filter((_, i) => index !== i)
+    setAddresses(newArray)
+  }
+
+  return (
+    <ChainMutation
+      mutation={ADD_PARTY_ADMINS}
+      resultKey="addAdmins"
+      variables={{
+        address,
+        userAddresses: (userAddresses || '').map(s => s.trim())
+      }}
+      refetchQueries={[{ query: PARTY_QUERY, variables: { address } }]}
+      onCompleted={() => this.setState({ userAddresses: null })}
+    >
+      {(mutate, result) => (
+        <AddPartyAdminsContainer>
+          <Label>
+            Add Admins (click + to add multiple admins in 1 transactions)
+          </Label>
+          {userAddresses.map((address, index) => {
+            return (
+              <AddAdminInputContainer>
+                <AddAdminInput
+                  onChange={event => {
+                    setAddress(index, event.target.value)
+                  }}
+                />
+                {index > 0 && (
+                  <Button onClick={() => removeAddress(index)}>x</Button>
+                )}
+              </AddAdminInputContainer>
+            )
+          })}
+          <Add onClick={() => pushAddress('')}>+</Add>
+          {/* <textarea
               rows="5"
               onChange={e => this.setState({ userAddresses: e.target.value })}
               type="text"
               placeholder="0x... (one per line)"
             >
               {userAddresses}
-            </textarea>
-            <ChainMutationButton
-              analyticsId="AddAdmins"
-              onClick={mutate}
-              result={result}
-              preContent="Add admins"
-            />
-          </Form>
-        )}
-      </ChainMutation>
-    )
-  }
+            </textarea> */}
+          <ChainMutationButton
+            analyticsId="AddAdmins"
+            onClick={mutate}
+            result={result}
+            preContent="Add admins"
+          />
+        </AddPartyAdminsContainer>
+      )}
+    </ChainMutation>
+  )
 }
 
 export default AddAdmin
--- src/components/SingleEvent/Admin/AdminPanel.js
+++ src/components/SingleEvent/Admin/AdminPanel.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Label from '../../Forms/Label'
 import Clear from './Clear'
 import Finalize from './Finalize'
 import SetLimit from './SetLimit'
@@ -8,6 +9,11 @@ import AddAdmin from './AddAdmin'
 export default function AdminPanel({ party }) {
   return (
     <>
+      <Label>Finalize</Label>
+      <p>
+        Finalize ends the event and allows participants to withdraw. No one will
+        be able to be mark attended after you finalize!
+      </p>
       <Finalize party={party} />
       <Clear />
       <SetLimit />
