ae1a4f145b984548a06cf79baa51746b48eac46b
Jeff Lau <jeff-lau@live.com>
2019-03-21 15:34:06 +0800

    Split time and date

--- src/utils/parties.js
+++ src/utils/parties.js
@@ -60,3 +60,15 @@ export const filterParticipants = (selectedFilter, search) => participant => {
     participant.user.address.toLowerCase().includes(search)
   )
 }
+
+export const getDayAndTimeFromDate = isoString => {
+  const date = new Date(isoString)
+  const hours = date.getHours() * 60 * 60 * 1000
+  const minutes = date.getMinutes() * 60 * 1000
+  const day = date.setHours(0, 0, 0, 0)
+  return [day, hours + minutes]
+}
+
+export const getDateFromDayAndTime = (day, time) => {
+  return new Date(new Date(day).setHours(0, 0, 0, 0) + time)
+}
