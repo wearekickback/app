99ff2726ff06b0c41603cc36242e81e950e05225
Makoto <inouemak@googlemail.com>
2019-03-29 00:25:20 +0000

    call 'onboard' only if not mobile

--- src/components/Header/Assist.js
+++ src/components/Header/Assist.js
@@ -7,23 +7,30 @@ const Assist = ({ expectedNetworkId }) => {
     dappId: 'c212885d-e81d-416f-ac37-06d9ad2cf5af',
     networkId: expectedNetworkId
   })
-  assistInstance
-    .onboard()
-    .then(function(success) {
-      console.log({ success })
-      // User has been successfully onboarded and is ready to transact
-      // This means we can be sure of the follwing user properties:
-      //  - They are using a compatible browser
-      //  - They have a web3-enabled wallet installed
-      //  - The wallet is connected to the config-specified networkId
-      //  - The wallet is unlocked and contains at least `minimumBalance` in wei
-      //  - They have connected their wallet to the dapp, congruent with EIP1102
-    })
-    .catch(function(error) {
-      // The user exited onboarding before completion
-      // Will let you know which stage of onboarding the user reached when they exited
-      console.log('catch***')
-      console.log(error.msg)
-    })
+  assistInstance.getState().then(function(state) {
+    if (state.mobileDevice) {
+      console.log('this is mobile')
+    } else {
+      console.log('this is desktop')
+      assistInstance
+        .onboard()
+        .then(function(success) {
+          console.log({ success })
+          // User has been successfully onboarded and is ready to transact
+          // This means we can be sure of the follwing user properties:
+          //  - They are using a compatible browser
+          //  - They have a web3-enabled wallet installed
+          //  - The wallet is connected to the config-specified networkId
+          //  - The wallet is unlocked and contains at least `minimumBalance` in wei
+          //  - They have connected their wallet to the dapp, congruent with EIP1102
+        })
+        .catch(function(error) {
+          // The user exited onboarding before completion
+          // Will let you know which stage of onboarding the user reached when they exited
+          console.log('catch***')
+          console.log(error.msg)
+        })
+    }
+  })
 }
 export default Assist
