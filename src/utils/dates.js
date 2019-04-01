891716a10f445951d5044fe6e9334fcefd2ffefe
Jeff Lau <jeff-lau@live.com>
2019-04-01 16:13:40 +0800

    Remove date instantiation

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,7 +1,7 @@
 import moment from 'moment'
 
 export const toPrettyDate = strOrDate =>
-  moment(new Date(strOrDate)).format('MMM Do, YYYY @ H:mm a')
+  moment(strOrDate).format('MMM Do, YYYY @ H:mm a')
 
 export const getDayAndTimeFromDate = isoString => {
   const date = new Date(isoString)
