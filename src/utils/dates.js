9f4c9cd2f708ea5d8a162e957231ef2c7f006517
Jeff Lau <jeff-lau@live.com>
2019-04-04 18:56:43 +0100

    Fix dates

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -5,12 +5,12 @@ import { timezones } from 'react-timezone'
 >>>>>>> Timepicker (#122)
 
 export const toPrettyDate = strOrDate =>
-  moment.utc(strOrDate).format('MMM Do, YYYY @ H:mm a')
+  moment(strOrDate).format('MMM Do, YYYY @ H:mm a')
 
 export const getDayAndTimeFromDate = isoString => {
   const date = new Date(isoString)
-  const hours = date.getHours() * 60 * 60 * 1000
-  const minutes = date.getMinutes() * 60 * 1000
+  const hours = date.getUTCHours() * 60 * 60 * 1000
+  const minutes = date.getUTCMinutes() * 60 * 1000
   const day = date.setHours(0, 0, 0, 0)
   return [day, hours + minutes]
 }
@@ -18,7 +18,7 @@ export const getDayAndTimeFromDate = isoString => {
 export const getDateFromDayAndTime = (day, time) => {
   const utc = Date.UTC(day.getFullYear(), day.getMonth(), day.getDay())
   const date = new Date(utc + time)
-  return date
+  return date.toUTCString()
 }
 
 export function getLocalTimezoneOffset() {
