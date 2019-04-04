91696b6d7fa49d9f4475f6eacf0711c8d157833c
Jeff Lau <jeff-lau@live.com>
2019-04-05 03:06:14 +0900

    Fix dates again

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,12 +1,13 @@
 import moment from 'moment'
 
 export const toPrettyDate = strOrDate =>
-  moment(strOrDate).format('MMM Do, YYYY @ H:mm a')
+  moment.utc(strOrDate).format('MMM Do, YYYY @ H:mm a')
 
 export const getDayAndTimeFromDate = isoString => {
   const date = new Date(isoString)
-  const hours = date.getUTCHours() * 60 * 60 * 1000
-  const minutes = date.getUTCMinutes() * 60 * 1000
+  console.log(date.toUTCString())
+  const hours = date.getHours() * 60 * 60 * 1000
+  const minutes = date.getMinutes() * 60 * 1000
   const day = date.setHours(0, 0, 0, 0)
   return [day, hours + minutes]
 }
@@ -14,7 +15,7 @@ export const getDayAndTimeFromDate = isoString => {
 export const getDateFromDayAndTime = (day, time) => {
   const utc = Date.UTC(day.getFullYear(), day.getMonth(), day.getDay())
   const date = new Date(utc + time)
-  return date.toUTCString()
+  return date
 }
 
 export function getLocalTimezoneOffset() {
