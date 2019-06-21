7d7acf14a1f67082cbae791e2f9588cb4f54cf2d
Jeff Lau <jeff-lau@live.com>
2019-06-21 15:27:26 +0800

    Add profile image

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -5,18 +5,21 @@ import EventAttended from './EventAttended'
 import { H2, H3, P } from '../Typography/Basic'
 
 const UserProfileContainer = styled('article')``
+const Avatar = styled('img')`
+  border-radius: 50%;
+`
 
 export default function UserProfile({ profile: p }) {
   const twitter = getSocialId(p.social, 'twitter')
   return (
     <UserProfileContainer>
-      <H2>{p.id}</H2>
-      <P>{p.username}</P>
+      <Avatar src={`https://avatars.io/twitter/${twitter}`} />
+      <H2>{p.username}</H2>
       {twitter && (
         <a href={`https://twitter.com/${twitter}`}>Twitter: {twitter}</a>
       )}
 
-      <H3>Events Attended ({eventsAttended.length})</H3>
+      <H3>Events Attended ({p.eventsAttended.length})</H3>
       {p.eventsAttended.map(event => (
         <EventAttended event={event} />
       ))}
