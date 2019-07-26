8b8721009ad6ed0b40fe1ebc4380b4e93c17858d
Jeff Lau <jeff-lau@live.com>
2019-07-26 14:50:04 +0800

    style user profile

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -4,29 +4,42 @@ import { getSocialId } from '@wearekickback/shared'
 import EventAttended from './EventAttended'
 import { H2, H3 } from '../Typography/Basic'
 
-const UserProfileContainer = styled('article')``
+const UserProfileWrapper = styled('div')`
+  display: flex;
+  justify-content: center;
+`
+
+const UserProfileContainer = styled('article')`
+  max-width: 500px;
+  padding: 20px;
+  background: #efefef;
+  border-radius: 20px;
+`
 const Avatar = styled('img')`
   border-radius: 50%;
+  max-width: 250px;
 `
 
 export default function UserProfile({ profile: p }) {
   const twitter = getSocialId(p.social, 'twitter')
   return (
-    <UserProfileContainer>
-      <Avatar src={`https://avatars.io/twitter/${twitter}`} />
-      <H2>{p.username}</H2>
-      {twitter && (
-        <a href={`https://twitter.com/${twitter}`}>Twitter: {twitter}</a>
-      )}
+    <UserProfileWrapper>
+      <UserProfileContainer>
+        <Avatar src={`https://avatars.io/twitter/${twitter}`} />
+        <H2>{p.username}</H2>
+        {twitter && (
+          <a href={`https://twitter.com/${twitter}`}>Twitter: {twitter}</a>
+        )}
 
-      <H3>Events Attended ({p.eventsAttended.length})</H3>
-      {p.eventsAttended.map(event => (
-        <EventAttended event={event} />
-      ))}
-      <H3>Events Hosted ({p.eventsHosted.length})</H3>
-      {p.eventsHosted.map(event => (
-        <EventAttended event={event} />
-      ))}
-    </UserProfileContainer>
+        <H3>Events Attended ({p.eventsAttended.length})</H3>
+        {p.eventsAttended.map(event => (
+          <EventAttended event={event} />
+        ))}
+        <H3>Events Hosted ({p.eventsHosted.length})</H3>
+        {p.eventsHosted.map(event => (
+          <EventAttended event={event} />
+        ))}
+      </UserProfileContainer>
+    </UserProfileWrapper>
   )
 }
