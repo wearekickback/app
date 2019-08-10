1f247c96e8a89c0838289e63677be6a162990ebc
Makoto <inouemak@googlemail.com>
2019-08-10 20:40:49 +0100

    Add DAI token address on Kovan

--- src/api/web3.js
+++ src/api/web3.js
@@ -164,6 +164,8 @@ export async function getTokenBySymbol(symbol) {
       return '' // TODO
     case '4':
       return '' // TODO
+    case '42':
+      return '0xc4375b7de8af5a38a93548eb8453a498222c4ff2'
     default:
       return DAI_CONTRACT_ADDRESS
   }
