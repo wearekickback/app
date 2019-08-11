2c0ea3a0f836bc91be14e965157ccb4fc4651aa3
Makoto <inouemak@googlemail.com>
2019-08-11 10:21:11 +0100

    Use abi which has sbynol as bytes32

--- src/api/resolvers/tokenResolvers.js
+++ src/api/resolvers/tokenResolvers.js
@@ -1,12 +1,141 @@
 import getWeb3, { getAccount, getTokenBySymbol } from '../web3'
-import { Token } from '@wearekickback/contracts'
+// import { Token } from '@wearekickback/contracts'
+
 import { txHelper, isEmptyAddress } from '../utils'
 export const defaults = {}
-const abi = Token.abi
+// This is because some token uses string as symbol while others are bytes32
+// We need some workaround like https://ethereum.stackexchange.com/questions/58945/how-to-handle-both-string-and-bytes32-method-returns when supporting any ERC20
+const detailedERC20bytes32ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'name',
+    outputs: [{ name: '', type: 'bytes32' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: 'spender', type: 'address' },
+      { name: 'value', type: 'uint256' }
+    ],
+    name: 'approve',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: 'from', type: 'address' },
+      { name: 'to', type: 'address' },
+      { name: 'value', type: 'uint256' }
+    ],
+    name: 'transferFrom',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'decimals',
+    outputs: [{ name: '', type: 'uint8' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [{ name: 'who', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'symbol',
+    outputs: [{ name: '', type: 'bytes32' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: 'to', type: 'address' },
+      { name: 'value', type: 'uint256' }
+    ],
+    name: 'transfer',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      { name: 'owner', type: 'address' },
+      { name: 'spender', type: 'address' }
+    ],
+    name: 'allowance',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [
+      { name: '_name', type: 'string' },
+      { name: '_symbol', type: 'string' },
+      { name: '_decimals', type: 'uint8' }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: 'owner', type: 'address' },
+      { indexed: true, name: 'spender', type: 'address' },
+      { indexed: false, name: 'value', type: 'uint256' }
+    ],
+    name: 'Approval',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: 'from', type: 'address' },
+      { indexed: true, name: 'to', type: 'address' },
+      { indexed: false, name: 'value', type: 'uint256' }
+    ],
+    name: 'Transfer',
+    type: 'event'
+  }
+]
+
 let token
 
 const getTokenContract = (web3, address) => {
-  return new web3.eth.Contract(abi, address).methods
+  return new web3.eth.Contract(detailedERC20bytes32ABI, address).methods
 }
 
 const resolvers = {
@@ -40,12 +169,13 @@ const resolvers = {
       const contract = getTokenContract(web3, tokenAddress)
 
       try {
-        const [name, symbol, decimals] = await Promise.all([
+        let [name, symbol, decimals] = await Promise.all([
           contract.name().call(),
           contract.symbol().call(),
           contract.decimals().call()
         ])
-
+        name = web3.utils.toAscii(name)
+        symbol = web3.utils.toAscii(symbol)
         return { name, symbol, decimals }
       } catch (err) {
         throw new Error(`Failed to get Token`)
