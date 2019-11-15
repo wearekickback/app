9547a8d32eacc710a38357783f24f764094ff5c8
Shane Fontaine <shane6fontaine@gmail.com>
2019-11-15 10:07:30 -0800

    Remove styled components in favor of emotion

--- src/components/Utils/LoadingDots.js
+++ src/components/Utils/LoadingDots.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'react-emotion'
 
-const Dots = styled.div`
+const Dots = styled('div')`
   padding-right: 1em;
 
   &::after {
