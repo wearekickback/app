7d47438f6d91372652ba8060614af9147eac915a
Makoto <inouemak@googlemail.com>
2019-12-22 18:46:41 -0500

    Remove mixpanel from rinkeby

--- scripts/setup.js
+++ scripts/setup.js
@@ -43,7 +43,6 @@ if (argv.ropsten) {
   appConfig.GIT_COMMIT = getGitCommit()
   appConfig.ROLLBAR_TOKEN = 'e676d64e462b48d098a12db8a173598a'
   appConfig.BLOCKNATIVE_DAPPID = '27b3eac2-e46c-428a-9a0c-56cce2725d42'
-  appConfig.MIXPANEL_ID = '28243587317e7b2d8a669dcce23302cb'
 } else if (argv.kovan) {
   appConfig.ENV = 'kovan'
   appConfig.API_URL = 'https://kovan.api.kickback.events'
