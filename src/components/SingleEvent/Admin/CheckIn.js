151dce0b45ac6e42ed189b21de4ff9d5831045db
Tom French <tom@tomfren.ch>
2020-02-19 13:30:23 +0000

    fix emotion import in CheckIn.js

--- src/components/SingleEvent/Admin/CheckIn.js
+++ src/components/SingleEvent/Admin/CheckIn.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import styled from 'react-emotion'
+import styled from '@emotion/styled'
 
 import Button from '../../Forms/Button'
 import TextInput from '../../Forms/TextInput'
