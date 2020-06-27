e973bbb4df9f93bbef8b97cd68c80cbe18c5bf76
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-06-27 18:32:25 +0100

    Do not show create page if not whitelisted

--- src/routes/CreateEvent.js
+++ src/routes/CreateEvent.js
@@ -3,9 +3,11 @@ import styled from '@emotion/styled'
 import { withRouter } from 'react-router-dom'
 import DefaultTextInput from '../components/Forms/TextInput'
 import Label from '../components/Forms/Label'
-
+import SafeQuery from '../components/SafeQuery'
+import { GlobalConsumer } from '../GlobalState'
 import PartyForm from '../components/SingleEvent/Admin/PartyForm'
 import { CREATE_PENDING_PARTY } from '../graphql/mutations'
+import { IS_WHITELISTED } from '../graphql/queries'
 
 const CreateContainer = styled('div')`
   display: flex;
@@ -26,21 +28,39 @@ class Create extends Component {
     const { password } = this.state
 
     return (
-      <CreateContainer>
-        <PartyForm
-          onCompleted={this._onCreated}
-          mutation={CREATE_PENDING_PARTY}
-          variables={{ password }}
-          type="create"
-        >
-          <Label>SECRET PASSWORD:</Label>
-          <TextInput
-            value={password}
-            onChangeText={val => this.setState({ password: val })}
-            type="password"
-          />
-        </PartyForm>
-      </CreateContainer>
+      <GlobalConsumer>
+        {({ userAddress }) => {
+          return (
+            <SafeQuery
+              query={IS_WHITELISTED}
+              variables={{ address: userAddress }}
+            >
+              {({ data: { isWhitelisted } }) => {
+                if (!isWhitelisted) {
+                  return <>To create an event, you have to be whitelisted</>
+                }
+                return (
+                  <CreateContainer>
+                    <PartyForm
+                      onCompleted={this._onCreated}
+                      mutation={CREATE_PENDING_PARTY}
+                      variables={{ password }}
+                      type="create"
+                    >
+                      {/* <Label>SECRET PASSWORD:</Label>
+                    <TextInput
+                      value={password}
+                      onChangeText={val => this.setState({ password: val })}
+                      type="password"
+                    /> */}
+                    </PartyForm>
+                  </CreateContainer>
+                )
+              }}
+            </SafeQuery>
+          )
+        }}
+      </GlobalConsumer>
     )
   }
 
