dd17b0c30e7bd091cb855258113d4d41493d89f8
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-09-25 16:52:17 +0100

    Participantslength and admin event (#444)
    * Make private events hidden
    
    * Add participantsLength

--- src/components/SingleEvent/EventParticipants.js
+++ src/components/SingleEvent/EventParticipants.js
@@ -21,7 +21,6 @@ import { HttpLink } from 'apollo-link-http'
 import _ from 'lodash'
 import { parseAvatar, participantsLength } from '../../api/utils'
 import { fetchAndSetPoapAddresses } from './utils'
-
 const graphClient = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({ uri: 'https://hub.snapshot.org/graphql' })
